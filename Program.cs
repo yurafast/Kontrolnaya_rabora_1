605315c85a26be37b4a645e4ef01de0192cb4641
“yurafast” <yurafm@gmail.com>
2022-06-26 12:18:59 +0300

    add code of program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+void Print(string[] array)
+{
+    int size= array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+int size = 5;
+string[] arr = new string[size];
+string[] res = new string[size];
+
+    for (int i = 0; i < size; i++) // Заполняем массив arr элементами
+    {
+        Console.WriteLine($"Введите {i}-й элемент");
+        arr[i] = Console.ReadLine();
+    }
+
+    int check; // переменная для проверки длинны элемента
+
+    for (int i = 0; i < size; i++) // заполняем массив res по условию задачи
+    {
+        check=(arr[i].Length);
+        if (check<4)
+        {
+            res[i]=arr[i];
+        }   
+    }
+
+Print(res);
+
